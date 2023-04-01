83ebafb1cd65ec5cbcd7f775447980ebec09bb73
Jared Johnson <jjohnson@axcient.com>
2023-04-01 02:49:29 -0500

    Put mvp_cli back but probably broken

--- src/mvp_cli.py
+++ src/mvp_cli.py
@@ -19,5 +19,28 @@ def main():
         print("Invalid choice.")
         return
 
+    print_reminder()
+
+    print("Paste the CONTENT BLOCK here and add a line with '# NO MORE FILES FROM CHATGPT' at the end:")
+    content_block = ""
+    for line in sys.stdin:
+        content_block += line
+        if line.strip() == "# NO MORE FILES FROM CHATGPT":
+            break
+
+    file_contents = parse_content_block(content_block)
+    write_files(file_contents)
+
+    command_block = input("Paste the COMMAND BLOCK here (or press Enter to skip):\n")
+    if command_block:
+        commands = parse_command_block(command_block)
+        output = execute_commands(commands)
+        print("\n# RESULTS")
+        for result in output:
+            print(result)
+
+    print("\n# DONE")
+
 if __name__ == "__main__":
     main()
+
