13d5bd806e93f392f819aa464cc74b410179a455
Jared Johnson <jjohnson@axcient.com>
2023-04-02 13:23:19 -0500

    Remove previous image before building image

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -3,6 +3,9 @@
 # Set your desired image name
 IMAGE_NAME="inception"
 
+# Stop and remove any existing container with the same name
+docker rm -f $IMAGE_NAME 2>/dev/null || true
+
 # Build the Docker image
 docker build -t $IMAGE_NAME .
 
