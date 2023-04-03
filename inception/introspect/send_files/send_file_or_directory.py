33b3d5bb39c8910212c079803307f406bef5183c
Jared Johnson <jjohnson@axcient.com>
2023-04-03 01:43:05 -0500

    Fix path names in isend

--- inception/introspect/send_files/send_file_or_directory.py
+++ inception/introspect/send_files/send_file_or_directory.py
@@ -1,13 +1,13 @@
 import os
-from .send_directory import send_directory
 from .file_contents import get_file_contents
+from .send_directory import send_directory
 
-def send_file_or_directory(file_path):
-    if os.path.isdir(file_path):
-        send_directory(file_path)
-    elif os.path.isfile(file_path):
-        print(f"## WRITE THIS FILE {file_path}")
-        content = get_file_contents(file_path)
-        if content is not None:
-            print(content)
-
+def send_file_or_directory(path, fallback_encoding='utf-8'):
+    if os.path.isfile(path):
+        print(f"## WRITE THIS FILE {path}")
+        content = get_file_contents(path, fallback_encoding)
+        if content is None:
+            return
+        print(content)
+    elif os.path.isdir(path):
+        send_directory(path, prefix=path, fallback_encoding=fallback_encoding)
