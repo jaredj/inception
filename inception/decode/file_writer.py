9a962ebb642403bba5f13aa32382d6389964f6a9
Jared Johnson <jjohnson@axcient.com>
2023-04-03 11:11:33 -0500

    Fix decoding files destined for top level

--- inception/decode/file_writer.py
+++ inception/decode/file_writer.py
@@ -3,8 +3,9 @@ import os
 def write_files(file_contents):
     for file_path, content_lines in file_contents.items():
         directory = os.path.dirname(file_path)
-        os.makedirs(directory, exist_ok=True)
+
+        if directory:
+            os.makedirs(directory, exist_ok=True)
 
         with open(file_path, 'w') as f:
             f.write('\n'.join(content_lines))
-
