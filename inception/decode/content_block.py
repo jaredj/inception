d8bc80763278ea7d98a1a40099c1a769c595fe06
Jared Johnson <jjohnson@axcient.com>
2023-04-03 01:01:03 -0500

    Move content block decoding to libary functions
    Removes command blocks which chatGPT really doesn't get

--- inception/decode/content_block.py
+++ inception/decode/content_block.py
@@ -1,8 +1,13 @@
+# inception/decode/content_block.py
 import sys
 from .file_parser import parse_content_block
 from .file_writer import write_files
 
-def decode_content_block():
+def decode_content_block(content_block):
+    file_contents = parse_content_block(content_block)
+    write_files(file_contents)
+
+def prompt_and_decode_content_block():
     print("Paste the CONTENT BLOCK here and add a line with '# NO MORE FILES FROM CHATGPT' at the end:")
     content_block = ""
     for line in sys.stdin:
@@ -10,8 +15,8 @@ def decode_content_block():
         if line.strip() == "# NO MORE FILES FROM CHATGPT":
             break
 
-    file_contents = parse_content_block(content_block)
-    write_files(file_contents)
+    decode_content_block(content_block)
 
 if __name__ == "__main__":
-    decode_content_block()
+    prompt_and_decode_content_block()
+
