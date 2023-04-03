ec43d60133514132680914cac175924df30dd195
Jared Johnson <jjohnson@axcient.com>
2023-04-03 11:14:30 -0500

    Loop content decoding
    Use `# DONE` to end

--- inception/decode/content_block.py
+++ inception/decode/content_block.py
@@ -1,4 +1,3 @@
-# inception/decode/content_block.py
 import sys
 from .file_parser import parse_content_block
 from .file_writer import write_files
@@ -8,15 +7,17 @@ def decode_content_block(content_block):
     write_files(file_contents)
 
 def prompt_and_decode_content_block():
-    print("Paste the CONTENT BLOCK here and add a line with '# NO MORE FILES FROM CHATGPT' at the end:")
-    content_block = ""
-    for line in sys.stdin:
-        content_block += line
-        if line.strip() == "# NO MORE FILES FROM CHATGPT":
-            break
+    while True:
+        print("Paste the CONTENT BLOCK here and add a line with '# NO MORE FILES FROM CHATGPT' at the end:")
+        content_block = ""
+        for line in sys.stdin:
+            content_block += line
+            if line.strip() == "# NO MORE FILES FROM CHATGPT":
+                break
+            if line.strip() == "# DONE":
+                return
 
-    decode_content_block(content_block)
+        decode_content_block(content_block)
 
 if __name__ == "__main__":
     prompt_and_decode_content_block()
-
